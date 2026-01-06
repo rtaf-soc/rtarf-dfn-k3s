3f7bda4355ed692429f4e6b6b13ae0897a83a7a5
pjames <pjames@gmail.com>
2026-01-06 15:19:12 +0700

    Setup addons

--- 02-initial-addons.bash
+++ 02-initial-addons.bash
@@ -2,37 +2,18 @@
 
 export $(xargs <.env)
 
-DST_DIR=addons
-
 # ต้องเพิ่ม volume map ตรง k3s volume ด้วย
 SRC_FILE=00-configs/addons-argocd.yaml
-echo "Copying file [${SRC_FILE}] to directory [${DST_DIR}]"
-cp ${SRC_FILE} ${DST_DIR}
+kubectl apply -f ${SRC_FILE}
 
 SRC_FILE=00-configs/addons-ingress.yaml
-echo "Copying file [${SRC_FILE}] to directory [${DST_DIR}]"
-cp ${SRC_FILE} ${DST_DIR}
+kubectl apply -f ${SRC_FILE}
 
 SRC_FILE=00-configs/addons-external-secret.yaml
-echo "Copying file [${SRC_FILE}] to directory [${DST_DIR}]"
-cp ${SRC_FILE} ${DST_DIR}
+kubectl apply -f ${SRC_FILE}
 
 SRC_FILE=00-configs/addons-cert-manager.yaml
-echo "Copying file [${SRC_FILE}] to directory [${DST_DIR}]"
-cp ${SRC_FILE} ${DST_DIR}
+kubectl apply -f ${SRC_FILE}
 
 SRC_FILE=00-configs/addons-eck.yaml
-echo "Copying file [${SRC_FILE}] to directory [${DST_DIR}]"
-cp ${SRC_FILE} ${DST_DIR}
-
-################ Gitea here ################
-#SRC_FILE=00-configs/addons-gitea.yaml
-
-# Replace credential variables in the file
-#echo "Replacing credentials in [${SRC_FILE}]"
-#sed -i "s/<<GIT_USER>>/${GIT_USER}/g" ${SRC_FILE}
-#sed -i "s/<<GIT_PASSWORD>>/${GIT_PASSWORD}/g" ${SRC_FILE}
-#sed -i "s/<<GIT_EMAIL>>/${GIT_EMAIL}/g" ${SRC_FILE}
-
-#echo "Copying file [${SRC_FILE}] to directory [${DST_DIR}]"
-#cp ${SRC_FILE} ${DST_DIR}
+kubectl apply -f ${SRC_FILE}
