89cc8aa63d214ae0a04267ba6f92493059b221a6
pjames <pjames@gmail.com>
2026-01-06 07:57:01 +0700

    Used entrypoint.sh for Suricata

---
+++ 11-configs-suricata/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+RULE_FILE="/var/lib/suricata/rules/suricata.rules"
+
+if [ ! -f "$RULE_FILE" ]; then
+  echo "[*] Rules not found, running suricata-update..."
+  suricata-update
+else
+  echo "[*] Rules already exist, skip update"
+fi
+
+exec suricata -i enp2s0 -c /etc/suricata/suricata.yaml
