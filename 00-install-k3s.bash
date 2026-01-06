36d5ba3b0f21c53eeee7fd87344af4e77638b30e
pjames <pjames@gmail.com>
2026-01-06 16:07:30 +0700

    Added storage class

--- 00-install-k3s.bash
+++ 00-install-k3s.bash
@@ -1,21 +1,5 @@
 #/bin/bash
 
-sudo mkdir -p /etc/rancher/k3s/server/manifests
-
-# 2. สร้างไฟล์กำหนดค่า Storage Path
-sudo tee /etc/rancher/k3s/server/manifests/local-path-config.yaml > /dev/null <<EOF
-apiVersion: helm.cattle.io/v1
-kind: HelmChartConfig
-metadata:
-  name: local-path-provisioner
-  namespace: kube-system
-spec:
-  valuesContent: |-
-    configData:
-      paths:
-        - /data
-EOF
-
-curl -sfL https://get.k3s.io | sh -s - --disable traefik
+curl -sfL https://get.k3s.io | sh -s - --default-local-storage-path "/data" --disable traefik
 
 # TODO : Copy k3s.yaml to home directory for kubectl usage
