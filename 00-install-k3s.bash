ed353d87c66af35f1f7ebe7a08fceb9079403c69
pjames <pjames@gmail.com>
2026-01-06 16:09:18 +0700

    Added storage class

--- 00-install-k3s.bash
+++ 00-install-k3s.bash
@@ -3,3 +3,6 @@
 curl -sfL https://get.k3s.io | sh -s - --default-local-storage-path "/data" --disable traefik
 
 # TODO : Copy k3s.yaml to home directory for kubectl usage
+sudo cp /etc/rancher/k3s/k3s.yaml $HOME/k3s.yaml
+sudo chown $(id -u):$(id -g) $HOME/k3s.yaml
+
