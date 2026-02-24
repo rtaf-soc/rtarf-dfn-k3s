f9a0c8c92c7de82036784b6e9ad95bbcd3589fa7
pjames <pjames@gmail.com>
2026-02-24 18:23:18 +0700

    Added gitea

--- 03-boot-strap.bash
+++ 03-boot-strap.bash
@@ -7,4 +7,9 @@ cd 01-bootstrap
 kubectl apply -f argocd-app.yaml
 kubectl apply -f argocd-ing.yaml
 
+YAML_FILE="argocd-local-repo.yaml"
+sed -i "s|<<GITEA_USERNAME>>|${GIT_USER}|g" ${YAML_FILE}
+sed -i "s|<<GITEA_PASSWORD>>|${GIT_PASSWORD}|g" ${YAML_FILE}
+kubectl apply -f ${YAML_FILE}
+
 cd ..
