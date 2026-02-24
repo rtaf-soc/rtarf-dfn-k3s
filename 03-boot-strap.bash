2531c9e9f5dc901f27a5f745e9d1acbcfcde3d02
pjames <pjames@gmail.com>
2026-02-24 18:33:53 +0700

    Added repo secrets

--- 03-boot-strap.bash
+++ 03-boot-strap.bash
@@ -8,8 +8,9 @@ kubectl apply -f argocd-app.yaml
 kubectl apply -f argocd-ing.yaml
 
 YAML_FILE="argocd-local-repo.yaml"
-sed -i "s|<<GITEA_USERNAME>>|${GIT_USER}|g" ${YAML_FILE}
-sed -i "s|<<GITEA_PASSWORD>>|${GIT_PASSWORD}|g" ${YAML_FILE}
-kubectl apply -f ${YAML_FILE}
+cp ${YAML_FILE} ${YAML_FILE}.tmp
+sed -i "s|<<GITEA_USERNAME>>|${GIT_USER}|g" ${YAML_FILE}.tmp
+sed -i "s|<<GITEA_PASSWORD>>|${GIT_PASSWORD}|g" ${YAML_FILE}.tmp
+kubectl apply -f ${YAML_FILE}.tmp
 
 cd ..
