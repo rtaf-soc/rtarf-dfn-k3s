7937934d20eba07caad0d4e5bce27143a868c3bb
pjames <pjames@gmail.com>
2026-02-24 19:36:56 +0700

    Refactored code

--- 03-boot-strap.bash
+++ 03-boot-strap.bash
@@ -4,10 +4,13 @@ export $(xargs <.env)
 
 cd 01-bootstrap
 
-kubectl apply -f argocd-app.yaml
 kubectl apply -f argocd-ing.yaml
 
 kubectl apply -f argocd-bootstrap-data-plane.yaml
+kubectl apply -f argocd-bootstrap-control-plane-dev.yaml
+kubectl apply -f argocd-bootstrap-control-plane-prod.yaml
+
+kubectl apply -f argocd-remote-repo.yaml
 
 YAML_FILE="argocd-local-repo.yaml"
 cp ${YAML_FILE} ${YAML_FILE}.tmp
