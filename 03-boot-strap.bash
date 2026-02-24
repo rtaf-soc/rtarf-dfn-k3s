4efc811e44630bb41bd90e8765b50f1e32858f29
pjames <pjames@gmail.com>
2026-02-24 19:24:10 +0700

    Refactored code

--- 03-boot-strap.bash
+++ 03-boot-strap.bash
@@ -7,6 +7,8 @@ cd 01-bootstrap
 kubectl apply -f argocd-app.yaml
 kubectl apply -f argocd-ing.yaml
 
+kubectl apply -f argocd-bootstrap-data-plane.yaml
+
 YAML_FILE="argocd-local-repo.yaml"
 cp ${YAML_FILE} ${YAML_FILE}.tmp
 sed -i "s|<<GITEA_USERNAME>>|${GIT_USER}|g" ${YAML_FILE}.tmp
