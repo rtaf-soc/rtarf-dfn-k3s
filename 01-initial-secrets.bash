21e5dadc0c80c6a0804624b733494c59aac32fc7
pjames <pjames@gmail.com>
2026-03-10 06:56:55 +0700

    Added secret provision waiting logic

--- 01-initial-secrets.bash
+++ 01-initial-secrets.bash
@@ -14,6 +14,17 @@ GIT_NS=gitea
 # สร้าง secret ใน namespace gitea โดยนำค่ามาจาก initial-secret จาก default namespace
 kubectl create namespace ${GIT_NS} --dry-run=client -o yaml | kubectl apply -f -
 
+
+# รอให้ secret initial-secret มี key GIT_USER ก่อนที่จะสร้าง secret gitea-admin-secret
+# ที่ต้องรอเพราะว่าบางที job initial-secret ใช้เวลานานในการไปดึง image แล้วสร้าง secret เสร็จ
+echo "Waiting for secret initial-secret with key GIT_USER..."
+until kubectl get secret initial-secret -n default \
+  -o jsonpath='{.data.GIT_USER}' 2>/dev/null | grep -q .; do
+  sleep 2
+done
+echo "Secret and key found"
+
+
 GIT_USER=$(kubectl get secret initial-secret -n default -o jsonpath='{.data.GIT_USER}' | base64 -d)
 GIT_PASSWORD=$(kubectl get secret initial-secret -n default -o jsonpath='{.data.GIT_PASSWORD}' | base64 -d)
 
